7473c272eb16ff7142e36454951131c2152c4eaa
L001-F <121824965+L001-F@users.noreply.github.com>
2024-11-30 16:01:46 +0100

    Delete view/role.js

--- view/role.js
+++
@@ -1,8 +0,0 @@
-
-document.getElementById('student-card').addEventListener('click', function() {
-    window.location.href = 'student.html';
-});
-
-document.getElementById('admin-card').addEventListener('click', function() {
-    window.location.href = 'admin.html';
-});
